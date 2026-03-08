0c04c3adfb639af0e249b8203d3fbe833d9d6482
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-03-08 01:22:09 -0600

    Update PrepairVM-UbuntuVersion.sh
    fixed the bootstrap script

--- PrepairVM-UbuntuVersion.sh
+++ PrepairVM-UbuntuVersion.sh
@@ -6,12 +6,12 @@ sudo apt update && apt upgrade -y
 sudo apt install qemu-guest-agent -y 
 #----------------------------------------------------
 curl -fsSL https://tailscale.com/install.sh | sudo sh
-systemctl enable --now tailscale
-tailscale set --operator=$USER
+sudo systemctl enable --now tailscaled
+sudo tailscale set --operator=$USER
 #----------------------------------------------------
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
 sudo apt install crowdsec -y
 
 # Harden SSH
 sudo cp ./sshd_config /etc/ssh/sshd_config
-sudo systemctl restart sshd
+sudo systemctl restart ssh
