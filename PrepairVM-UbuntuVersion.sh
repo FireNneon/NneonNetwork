b515e0636529b05e2526ec9a5862b36841cf5562
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-03-08 00:52:53 -0600

    Add files via upload
    added bootstrap sh

---
+++ PrepairVM-UbuntuVersion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update the VM Ubuntu and install qemu-guest-agent & tailscale, ETC. 
+sudo apt update && apt upgrade -y
+
+sudo apt install qemu-guest-agent -y 
+#----------------------------------------------------
+curl -fsSL https://tailscale.com/install.sh | sudo sh
+systemctl enable --now tailscale
+tailscale set --operator=$USER
+#----------------------------------------------------
+curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
+sudo apt install crowdsec -y
+
+# Harden SSH
+sudo cp ./sshd_config /etc/ssh/sshd_config
+sudo systemctl restart sshd
