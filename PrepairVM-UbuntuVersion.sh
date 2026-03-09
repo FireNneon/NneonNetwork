a125629bd10ce95b0e4a78969b6d1468aecba09c
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-03-08 23:26:28 -0500

    Update PrepairVM-UbuntuVersion.sh
    Added to the script to automatically give the system full disk.

--- PrepairVM-UbuntuVersion.sh
+++ PrepairVM-UbuntuVersion.sh
@@ -5,6 +5,9 @@ sudo -v
 
 # Update the VM Ubuntu and install qemu-guest-agent & tailscale, ETC. 
 sudo apt update && apt upgrade -y
+# Expand LVM to use full disk
+sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
 
 sudo apt install qemu-guest-agent -y 
 #----------------------------------------------------
