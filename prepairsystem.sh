09390ac5c8a61d220acaa698d52e2f3afaf386ea
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-03-14 17:27:37 -0500

    Update prepairsystem.sh
    added docker install choice to the VM script.

--- prepairsystem.sh
+++ prepairsystem.sh
@@ -9,7 +9,33 @@ echo " 2) LXC"
 echo "" 
 read -p "Enter choice [1-2]: " choice
 
+
 if [[ "$choice" == "1" ]]; then 
+     read -p "Do you want to install docker in this VM? (yes or no): " answer
+     if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+          # Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+#install docker
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sleep 2
+sudo systemctl enable --now docker     
+     fi
 
 # Expand LVM to use full disk
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
@@ -17,7 +43,7 @@ sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
 
 # Update the VM Ubuntu and install qemu-guest-agent & tailscale, ETC. 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl git -y
+sudo apt install curl -y
 
 sudo apt install qemu-guest-agent -y 
 #----------------------------------------------------
@@ -37,12 +63,11 @@ sudo systemctl daemon-reload
 sudo systemctl restart ssh
 
 echo "System prepaired for VM!! :}"
-
 elif [[ "$choice" == "2" ]]; then
 
 # Update the VM Ubuntu and install tailscale and what not. 
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl git -y
+sudo apt install curl -y
 #----------------------------------------------------
 curl -fsSL https://tailscale.com/install.sh | sudo sh
 sudo systemctl enable --now tailscaled
@@ -59,6 +84,7 @@ sudo cp ./sshd_config /etc/ssh/sshd_config
 sudo systemctl daemon-reload
 sudo systemctl restart ssh
 echo "System prepaired for LXC!! :}"
+
 else 
     echo "invalid choice, exiting."
     exit 1
