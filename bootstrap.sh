512f4caa9d207bb1fe93395b1ef3f0d30059e135
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 17:41:07 -0500

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,24 +2,26 @@
 source ./common.sh
 
 sudo -v || { echo "sudo authentication failed, exiting."; exit 1; }
-clear
 echo "------NneonNetwork--------"
 echo "Starting Bootstrap.sh...."
 echo "--------------------------"
 sleep 2.3
 clear
-echo "-----"
+echo "."
+clear
+echo ".."
+clear
 echo "..."
-echo "-----"
 sleep 2.3
-echo "-----"
-echo "...."
-echo "-----"
-sleep 2.3
+Clear
+echo ""
 echo "------------NneonNetwork-----------------"
 echo "Welcome. One moment, detecting System...."
 echo "-----------------------------------------"
+echo ""
 sleep 2.3
+echo ""
 echo "------NneonNetwork--------"
 echo "Detected System is $distro"
 echo "--------------------------"
+echo ""
