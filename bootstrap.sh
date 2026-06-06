aa262c882e94f15ee346cff95d60efa7b8295183
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 17:11:10 -0500

    WIP 6/6/26

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,10 @@ source ./common.sh
 
 sudo -v || { echo "sudo authentication failed, exiting."; exit 1; }
 
- 
- echo "What do you want todo with this system?"
-echo " 1) Only Debloat system"
-echo " 2) Prepare system?"
-echo ""
-read -rp "Enter choice [1-2] (type anything to cancel whole script): " doingtype
+echo "Starting Bootstrap.sh...."
+sleep 3
+echo "Welcome...."
+sleep 3 
+echo "One moment, detecting System...."
 sleep 2
-read -rp "What type of system is this, LXC or VM?: " systemtype
-sleep 2
+echo "Detected System is $distro"
