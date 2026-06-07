823b38419db3015c1ee7e3e3c9e03060ec04593b
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 20:33:17 -0500

    WIP Fixed up options messages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,23 @@ echo ".."
 sleep 2.2
 clear
 echo "..."
+sleep 2.2
+echo "L"
+sleep 2.1
+echo "Lo"
+sleep 2.1
+echo "Loa"
+sleep 2.1
+echo "Load"
+sleep 2.1
+echo "Loade"
+sleep 2.1
+echo "Loaded"
 sleep 2.3
 clear
+echo ""
 echo "------------NneonNetwork-----------------"
-echo "Welcome. My name is Nix, One moment, While I detect the system."
+echo "Welcome. My name is Nix, I'm so happy to meet you. One moment, While I detect the system."
 sleep 2.2
 echo "Detecting System...."
 echo "-----------------------------------------"
@@ -25,7 +38,7 @@ echo ""
 echo "------NneonNetwork--------"
 echo "The System I've detected is $distro"
 echo "--------------------------"
-read -rp "Is this correct? (Yes or No?)" correctdistro
+read -rp "Is this correct? (Yes or No?): " correctdistro
 echo ""
 case "$correctdistro" in
     Yes|yes|Y|y)
@@ -34,19 +47,19 @@ case "$correctdistro" in
         ;;
     No|no|N|n)
         echo "1) Manual override"
-        echo "2) Detection was Correct After All? "
+        echo "2) Detection was Correct After All"
         echo "3) Cancel"
-        read -rp "You have told me that the distro detected was wrong. Sorry to Hear this. Please Select an option to proceed..(1-3)" distrochoice 
+        read -rp "You have told me that the distro detected was wrong. Sorry to Hear this. Please Select an option to proceed..(1-3): " distrochoice 
         case "$distrochoice" in
             1)
                 echo "Manual Override Has been Selected..."
                 sleep 2.2
                 echo ""
-                read -rp "Please Tell me Which Distro You are using?" manual
+                read -rp "Please Tell me Which Distro You are using?: " manual
                 distro="$manual"
                 echo ""
                 while true; do 
-                read -rp "You have entered $distro, is this correct? (Yes | No)" correct
+                read -rp "You have entered $distro, is this correct? (Yes | No): " correct
                 case "$correct" in
                 
                     Yes|yes|Y|y)
@@ -55,7 +68,7 @@ case "$correctdistro" in
                         break
                         ;;
                     No|no|N|n)
-                        read -rp "Please Retype what distro you are using? Sorry that got it wrong." manual
+                        read -rp "Please Retype what distro you are using? Sorry that got it wrong.: " manual
                         distro="$manual"
                         ;;
                 esac
@@ -65,7 +78,7 @@ case "$correctdistro" in
                 echo "1) Uses dnf"
                 echo " 2) Uses apt"
                 echo " 3) other (doesn't use either)"
-                read -rp "Does $distro use dnf or apt? Please Select an Option. (1-3)" pkinfo
+                read -rp "Does $distro use dnf or apt? Please Select an Option. (1-3): " pkinfo
                 case "$pkinfo" in
                     1 | Rocky | rocky | Fedora | fedora | RHEL | rhel)
                         pk="dnf"
