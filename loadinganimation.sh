b474fe263fc0ce3a0d8cfd1e90f347d8e5fd1425
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 20:53:13 -0500

    WIP

--- loadinganimation.sh
+++ loadinganimation.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 LoadingAnimation() {
-	sleep 2.0
+	sleep 1.0
 	clear
 	echo ""
 	echo "."
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo ".."
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "..."
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...L"
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...Lo"
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...Loa"
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...Load"
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...Loade"
-	sleep 2.1
+	sleep 1.1
 	clear
 	echo ""
 	echo "...Loaded"
-	sleep 2.0
+	sleep 1.0
 	clear
 }
