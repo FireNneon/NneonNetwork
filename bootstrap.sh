58264d7100677f8d4607efc4d864d3e87f0506fe
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 20:42:34 -0500

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,17 +29,18 @@ sleep 2.3
 clear
 echo ""
 echo "------------NneonNetwork-----------------"
-echo "Welcome. My name is Nix, I'm so happy to meet you. One moment, While I detect the system."
+echo "Unknown: Welcome. My name is Nix, I'm so happy to meet you. One moment, While I detect the system."
 sleep 2.2
 echo "Detecting System...."
 echo "-----------------------------------------"
 sleep 2.3
 echo ""
-echo "------NneonNetwork--------"
-echo "The System I've detected is $distro"
-echo "--------------------------"
-read -rp "Is this correct? (Yes or No?): " correctdistro
+echo "------------NneonNetwork-----------------"
+echo "Nix: The System I've detected is $distro"
+echo "-----------------------------------------"
+read -rp "Nix: Is this correct? (Yes or No?): " correctdistro
 echo ""
+echo "------------NneonNetwork-----------------"
 case "$correctdistro" in
     Yes|yes|Y|y)
         echo "Thank you for confirming that it was correct, I'll now proceed with setting up this system..."
@@ -49,26 +50,27 @@ case "$correctdistro" in
         echo "1) Manual override"
         echo "2) Detection was Correct After All"
         echo "3) Cancel"
-        read -rp "You have told me that the distro detected was wrong. Sorry to Hear this. Please Select an option to proceed..(1-3): " distrochoice 
+        read -rp "Nix: You have told me that the distro detected was wrong. Sorry to Hear this. Please Select an option to proceed..(1-3): " distrochoice 
+        echo "------------NneonNetwork-----------------"
         case "$distrochoice" in
             1)
-                echo "Manual Override Has been Selected..."
+                echo "Nix: Manual Override Has been Selected..."
                 sleep 2.2
                 echo ""
-                read -rp "Please Tell me Which Distro You are using?: " manual
+                read -rp "Nix: Please Tell me Which Distro You are using?: " manual
                 distro="$manual"
                 echo ""
                 while true; do 
-                read -rp "You have entered $distro, is this correct? (Yes | No): " correct
+                read -rp "Nix: You have entered $distro, is this correct? (Yes | No): " correct
                 case "$correct" in
                 
                     Yes|yes|Y|y)
-                        echo "Thanks for confirming that I understood what you typed was correct. Now proceeding with setup..."
+                        echo "Nix: Thanks for confirming that I understood what you typed was correct. Now proceeding with setup..."
                         sleep 2.3
                         break
                         ;;
                     No|no|N|n)
-                        read -rp "Please Retype what distro you are using? Sorry that got it wrong.: " manual
+                        read -rp "Nix: Please Retype what distro you are using? Sorry that got it wrong.: " manual
                         distro="$manual"
                         ;;
                 esac
@@ -78,7 +80,7 @@ case "$correctdistro" in
                 echo "1) Uses dnf"
                 echo " 2) Uses apt"
                 echo " 3) other (doesn't use either)"
-                read -rp "Does $distro use dnf or apt? Please Select an Option. (1-3): " pkinfo
+                read -rp "Nix: Does $distro use dnf or apt? Please Select an Option. (1-3): " pkinfo
                 case "$pkinfo" in
                     1 | Rocky | rocky | Fedora | fedora | RHEL | rhel)
                         pk="dnf"
@@ -93,11 +95,11 @@ case "$correctdistro" in
                 ;;
             2)
 
-                echo "Ah I see, mistakes happen, proceeding..."
+                echo "Nix: Ah I see, mistakes happen, proceeding..."
                 sleep 2.3
                 ;;
             *) 
-                echo "Canceling script..."
+                echo "Nix: Canceling script..."
                 exit 1
                 ;;
 
