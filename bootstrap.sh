f71434052172a37426315e5279a6019c8ce56269
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-06-06 19:02:10 -0500

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,17 +16,19 @@ echo "..."
 sleep 2.3
 clear
 echo "------------NneonNetwork-----------------"
-echo "Welcome. One moment, detecting System...."
+echo "Welcome. My name is Nix, One moment, While I detect the system."
+sleep 2.2
+echo "Detecting System...."
 echo "-----------------------------------------"
 sleep 2.3
 echo ""
 echo "------NneonNetwork--------"
-echo "Detected System is $distro"
+echo "The System I've detected is $distro"
 echo "--------------------------"
 read -rp "Is this correct? (Yes or No?)" correctdistro
 case "$correctdistro" in
     Yes|yes|Y|y)
-    echo "Thank you for confirming that it was correct, now proceeding..."
+    echo "Thank you for confirming that it was correct, I'll now proceed with setting up this system..."
     sleep 2.3
         ;;
     No|no|N|n)
@@ -34,7 +36,7 @@ case "$correctdistro" in
         echo "2) Ubuntu"
         echo "3) Detection was Correct After All? "
         echo "4) Cancel"
-        read -rp "You have told use that the distro detected is wrong, out of these options which is correct? (1-4)" distrochoice 
+        read -rp "You have told me that the distro detected was wrong, sorry, out of these options which is correct? (1-4)" distrochoice 
         case "$distrochoice" in
             1)
                 distro="rocky"
@@ -66,3 +68,6 @@ case "$correctdistro" in
 esac
 
 echo "This message is for testing, the distro currently set is $distro and the PackageManager is $pk"
+sleep 2.3
+echo "This is to test if the functions changed" 
+first_steps
