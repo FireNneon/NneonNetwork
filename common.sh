9927d281b9d6769dffd40832af9100aaa86d6e59
Harold B <59723376+FireNneon@users.noreply.github.com>
2026-08-15 19:09:01 -0500

    WILL THIS FIX my tailscale install snippet??? :/

--- common.sh
+++ common.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+# shellcheck disable=SC2154
 source ./distros/rocky/rocky-swap.sh
 harden_ssh() {
 	#Harden SSH
+	
 	sudo cp ./distros/"$distro"/sshd_config /etc/ssh/sshd_config
 	sudo chown root:root /etc/ssh/sshd_config && sudo chmod 644 /etc/ssh/sshd_config
 	sudo systemctl daemon-reload
@@ -34,19 +35,14 @@ install_crowdsec() {
 }
 
 install_tailscale() {
-	#----------------------------------------------------
-	curl -fsSL https://tailscale.com/install.sh | sudo sh
-	sudo systemctl enable --now tailscaled
-	sudo tailscale set --operator="$USER"
-	sleep 2
-	#----------------------------------------------------
-	clear
 	echo "------------NneonNetwork-----------------"
-	read -rp "Nix: Okay I gotta stop the setup for now, I need know if you want me to setup Tailscale automatically for you or do you want todo it manually? (yes or no?): " tailscale_auto
-		if [[ "$tailscale_auto" = yes ]]; then
+	read -rp "Nix: Okay I gotta stop the setup for now, I need know if you want me to setup Tailscale automatically for you or do you want todo it manually? once I know this I'll install tailscale client either way. (yes or no?): " tailscale_auto
+	case "$tailscale_auto" in
+		Yes | yes | y)
+			
 			clear
 			echo "------------NneonNetwork-----------------"
-			echo "Nix: Okay, you want me to set it up for you, before I can do that, you need to answer some questions for me. :]: "
+			echo "Nix: Okay, you want me to set it up for you, before I can do that, you need to answer some questions for me. :]"
 			echo "-----------------------------------------"
 			sleep 2.1
 			clear
@@ -66,13 +62,30 @@ install_tailscale() {
 			echo "-----------------------------------------"
 			sleep 2.3
 			clear
-			sudo tailscale up --login-server="$ts_server" --authkey="$ts_authkey"
-		elif [[ "$tailscale_auto" = no ]]; then
+			;;
+		No | no | n)
+			
 			clear
 			echo "------------NneonNetwork-----------------"
 			echo "Nix: okay you want todo it manually. Continuing..."
 			echo "-----------------------------------------"
-		fi
+			sleep 2.3
+			clear
+			;;
+		*)
+			echo "------------NneonNetwork-----------------"
+			echo "Nix: Sadly I don't understand..., I'll just default to Manual and continue the script since we are so close to done...."
+			echo "-----------------------------------------"
+			;;
+	esac
+	#----------------------------------------------------
+	curl -fsSL https://tailscale.com/install.sh | sudo sh
+	sudo systemctl enable --now tailscaled
+	sudo tailscale set --operator="$USER"
+	sleep 2
+	#----------------------------------------------------
+	clear
+	sudo tailscale up --login-server="$ts_server" --authkey="$ts_authkey"
 }
 
 install_defaults() {
